466102196ddcab825da72ea55796f937f4cf2d19
yildizsoner <117564966+yildizsoner@users.noreply.github.com>
2022-12-20 19:48:29 +0300

    20

---
+++ src/advJavapractice/multithreads/semophorex/SemaphorePr.java
@@ -0,0 +1,14 @@
+package advJavapractice.multithreads.semophorex;
+/*
+Semaphore bir kaynağa erişebilen iş parçacığı sayısını kısıtlamak için kullanılır.
+Yani, synchronized sadece bir iş parçacığının kilitlenmesi ve
+senkronize blok / yöntemi yürütmesine izin verirken, Semaphore n iş parçacığına kadar izin verir
+ve diğerlerini engeller.
+
+senaryo:deneme kabini/ödeme kasası
+Mağazanın 3 deneme kabini var. 5 kişi mağazanın deneme kabinini
+kullanmak istiyor. Bu 5 tane işlem için 5 thread kullanılsın.
+*/
+public class SemaphorePr {
+
+}
---
+++ src/advJavapractice/multithreads/threadpool/AppointmentCenter2.java
@@ -0,0 +1,19 @@
+package advJavapractice.multithreads.threadpool;
+
+import java.time.LocalDate;
+
+public class AppointmentCenter2 {
+
+    private LocalDate day=LocalDate.now();
+
+    public synchronized LocalDate getAppointmentDate(){
+        day=day.plusDays(1);
+        return day;
+    }
+
+
+
+
+
+
+}
