7dba0c6c6e41de555a9e7c177080e93e64c23d83
yildizsoner <117564966+yildizsoner@users.noreply.github.com>
2023-01-06 22:19:26 +0300

    commit -m"v122" git commit -m"1111"

--- src/advJavapractice/multithreads/semophorex/SemaphorePr.java
+++ src/advJavapractice/multithreads/semophorex/SemaphorePr.java
@@ -1,4 +1,7 @@
 package advJavapractice.multithreads.semophorex;
+
+import java.util.concurrent.Semaphore;
+
 /*
 Semaphore bir kaynağa erişebilen iş parçacığı sayısını kısıtlamak için kullanılır.
 Yani, synchronized sadece bir iş parçacığının kilitlenmesi ve
@@ -10,5 +13,31 @@ Mağazanın 3 deneme kabini var. 5 kişi mağazanın deneme kabinini
 kullanmak istiyor. Bu 5 tane işlem için 5 thread kullanılsın.
 */
 public class SemaphorePr {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+
+    }
+
+}
+
+class Person extends Thread{
+    private String name ;
+    private Semaphore semaphore;
+
+    public Person(String name, Semaphore semaphore) {
+        this.name = name;
+        this.semaphore = semaphore;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(name+ " kabine girmek istiyor...");
+        try {
+            semaphore.acquire();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
